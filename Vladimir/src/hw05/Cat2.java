917b6fa41e1e6f27244d760e4188c61b0c6b2a65
wladymyrgood <wladymyrgood@gmail.com>
2022-12-15 11:00:45 +0200

    my commit 3

--- Vladimir/src/hw05/Cat2.java
+++
@@ -1,6 +0,0 @@
-package src.hw05;
-
-public class Cat2 {
-    public static int id;
-
-}
--- Vladimir/src/hw05/Serv1.java
+++ Vladimir/src/hw05/Serv1.java
@@ -4,7 +4,7 @@ public class Serv1 {
     public static void main(String[] args) {
 
         Cat1 cat1 = new Cat1();
-        Cat2 cat2 = new Cat2();
+
 
 
     }
