b32b80fb0ca721e9fa517b715fd14f152b174782
wladymyrgood <wladymyrgood@gmail.com>
2022-12-30 01:26:03 +0200

    less9

---
+++ Vladimir/src/less9/Massif2.java
@@ -0,0 +1,30 @@
+package src.less9;
+
+import java.util.Arrays;
+
+public class Massif2 {
+    public int id;
+    public String name;
+
+    public Massif2(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+       /* Massif2[] massif = new Massif2[6];
+        Massif2[] massif2 = new Massif2[5];
+        Massif2[] massif3 = new Massif2[4];
+
+        System.out.println(Arrays.toString(new int[]{massif.length}));
+        System.out.println(Arrays.toString(new int[]{massif2.length}));
+        System.out.println(Arrays.toString(new int[]{massif3.length}));*/
+
+
+
+    }
+}
+
+
+
+
---
+++ Vladimir/src/less9/Massif3.java
@@ -0,0 +1,35 @@
+package src.less9;
+
+import java.util.Arrays;
+
+public class Massif3 {
+    public static void main(String[] args) {
+      /* Massif2[] massif = new Massif2[6];
+        Massif2[] massif2 = new Massif2[5];
+        Massif2[] massif3 = new Massif2[4];
+        System.out.println(Arrays.toString(new int[]{massif.length}));
+        System.out.println(Arrays.toString(new int[]{massif2.length}));
+        System.out.println(Arrays.toString(new int[]{massif3.length}));
+
+        int[] numbers = new int[5];
+        numbers[0] = 11;
+        numbers[1] = 22;
+        numbers[2] = 33;
+        numbers[3] = 44;
+        System.out.println(Arrays.toString(numbers));
+
+        numbers = Arrays.copyOf(numbers, numbers.length + 3 / 2 + 1);
+        System.out.println(Arrays.toString(numbers));
+
+      /*  int maxNumber = 0;
+        int next = 44;
+        for (int number : numbers) {
+            if (number > maxNumber) {
+                maxNumber = number;
+            }
+        }
+        System.out.println("max number in array = " + maxNumber);*/
+    }
+
+}
+
