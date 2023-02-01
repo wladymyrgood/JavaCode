aa868b90d5f5c3c186a00fd5e23f3a4dddf065a0
wladymyrgood <wladymyrgood@gmail.com>
2023-02-01 21:55:41 +0200

    less17

---
+++ Vladimir/src/less17/Materials.java
@@ -0,0 +1,45 @@
+package src.less17;
+
+public class Materials {
+
+    private int id;
+    private String name;
+    private int lectureId;
+    private ResourceType ResourceType;
+
+    public Materials() {
+    }
+
+    public Materials(int id, String name, int lectureId, src.less17.ResourceType resourceType) {
+        this.id = id;
+        this.name = name;
+        this.lectureId = lectureId;
+        ResourceType = resourceType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLectureId() {
+        return lectureId;
+    }
+
+    public src.less17.ResourceType getResourceType() {
+        return ResourceType;
+    }
+
+    @Override
+    public String toString() {
+        return "Materials{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lectureId=" + lectureId +
+                ", ResourceType=" + ResourceType +
+                '}';
+    }
+}
